64df89afe682f02bc801e12be4299373e540539c
Fluch der Viktoria <105786783+FluchDerViktoria@users.noreply.github.com>
2023-03-20 20:33:42 +0100

    The small begin

---
+++ Client/FluchDerVika.WeeklySchedule.Client/Model/StickyNote.cs
@@ -0,0 +1,31 @@
+﻿using FluchDerVika.Wpf.Essentials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluchDerVika.WeeklySchedule.Client.Model
+{
+  public class StickyNote : NotifyObject
+  {
+    #region Properties
+
+    public int ID
+    {
+      get => GetProperty<int>();
+      set => SetProperty(value);
+    }
+
+    public string Name
+    {
+      get => GetProperty<string>();
+      set => SetProperty(value);
+    }
+
+    #endregion Properties
+    public StickyNote() 
+    { 
+    }
+  }
+}
---
+++ Client/FluchDerVika.WeeklySchedule.Client/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using FluchDerVika.WeeklySchedule.Client.Model;
+using FluchDerVika.Wpf.Essentials;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluchDerVika.WeeklySchedule.Client.ViewModel
+{
+  public class MainViewModel : NotifyObject
+  {
+    #region Properties
+
+    public ObservableCollection<StickyNote> StickyNotes
+    {
+      get => GetProperty<ObservableCollection<StickyNote>>();
+      set => SetProperty(value);
+    }
+
+    #endregion Properties
+    public MainViewModel()
+    {
+      StickyNotes = new ObservableCollection<StickyNote>();
+      FillTestData();
+    }
+
+    #region DEBUG
+
+    public void FillTestData()
+    {
+      // TODO
+    }
+
+    #endregion DEBUG
+  }
+}
